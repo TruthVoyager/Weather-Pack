7075f8749a8f052bf124392d86255e02db1709ed
Christian Jones <christian.jones@indwes.edu>
2020-10-03 17:04:06 -0400

    Started Production
    Implementing google places suggestions and passed the result to get place lat and long. The passed lat and long to weather API.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'design/App.css';
+import CityInputForm from './components/CityInputForm.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    //const [weatherData, setWeatherData] = useState([]);
+    const fetchPlaceLatLong = async (placeId) => {
+        const resp = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?place_id=${placeId}&key=AIzaSyA8Q3_mGxcTWk60zHUfNzM4wowesk2TGSE`);
+        const data = await resp.json();
+        fetchWeatherData(data); //returns an object. 
+    };
+    const fetchWeatherData = async (placeData) => {
+        const resp = await fetch(`api.openweathermap.org/data/2.5/forecast?lat=${placeData.results.geometry.location.lat}&lon=${placeData.results.geometry.location.lng}&appid=3e2bd5f93177f236d0f300f96299a06c`);
+        const data = await resp.json();
+        console.log(data);
+        //setWeatherData([data]); 
+    };
+    return (
+        <div className="App">
+            <header className="App-header">
+                <CityInputForm placeIdOut={fetchPlaceLatLong} />
+            </header>
+        </div>
+    );
 }
 
 export default App;
